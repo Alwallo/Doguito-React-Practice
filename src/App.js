92b0ceee57aaaf3606cac02cfd1e807269d1d93b
Alwallo <alvgonzalezb@gmail.com>
2023-05-04 09:44:27 -0600

    Paste del Page404

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Sobre from './pages/Sobre.jsx';
 import Header from './components/Header';
+import Page404 from './pages/Page404';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element=<Home></Home>></Route>
         <Route path='/sobre' element=<Sobre></Sobre>></Route>
+        <Route path='*' element=<Page404></Page404> ></Route>
       </Routes>
     </Router>
   );
