cf8a4ec3bbf4f792386c6904251e287719e89605
Alwallo <alvgonzalezb@gmail.com>
2023-05-04 09:39:21 -0600

    Paste del Header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './assets/css/componentes/card.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Sobre from './pages/Sobre.jsx';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+    <Header></Header> 
       <Routes>
         <Route path='/' element=<Home></Home>></Route>
         <Route path='/sobre' element=<Sobre></Sobre>></Route>
